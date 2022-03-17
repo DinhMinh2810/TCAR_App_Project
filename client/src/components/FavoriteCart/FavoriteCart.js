e3ec1f9b53f51cd97f9d5ee8f2c3af76c1f778d2
DinhMinh2810 <dinhminh28102000@gmail.com>
2022-03-17 08:36:09 +0700

    add favorite card fe

--- client/src/components/FavoriteCart/FavoriteCart.js
+++ client/src/components/FavoriteCart/FavoriteCart.js
@@ -6,6 +6,9 @@ import {
 } from '../../redux/actions/favoriteCartActions';
 import { FavoriteItemCard } from './FavouriteItemCard/FavoriteItemCard';
 import PriceCheckIcon from '@mui/icons-material/PriceCheck';
+import { ToastContainer, toast } from 'react-toastify';
+import TitleBarPage from './../Layout/TitleBarPage';
+import { Link } from 'react-router-dom';
 
 const FavoriteCart = () => {
 	const dispatch = useDispatch();
@@ -13,19 +16,16 @@ const FavoriteCart = () => {
 
 	const increaseQuantity = (id, quantity, available) => {
 		const newQuantity = quantity + 1;
-		if (available <= quantity) {
-			return;
-		} else {
-			dispatch(addCarsToCart(id, newQuantity));
-		}
+		dispatch(addCarsToCart(id, newQuantity));
 	};
 
 	const decreaseQuantity = (id, quantity) => {
 		const newQuantity = quantity - 1;
-		if (quantity <= 1) {
-			return;
+		if (newQuantity <= 0) {
+			toast.error('Cannot choose negative number of rental days !!');
+		} else {
+			dispatch(addCarsToCart(id, newQuantity));
 		}
-		dispatch(addCarsToCart(id, newQuantity));
 	};
 
 	const removeCarsCart = (id) => {
@@ -35,14 +35,31 @@ const FavoriteCart = () => {
 	return (
 		<>
 			{favoriteCartItems.length === 0 ? (
-				<h1>Chua co </h1>
+				<div className="w-screen flex items-center">
+					<div className="container flex flex-col  md:flex-row items-center justify-center text-gray-700 pt-40">
+						<div className="max-w-md md: px-4">
+							<div className="text-5xl font-dark font-bold">Empty 🤣🤣</div>
+							<p className="text-2xl md:text-3xl font-light leading-normal mb-8 mt-4">
+								You haven't added any cars to your favorite cart !!
+							</p>
+							<Link
+								to="/carProduct/refreshSearch"
+								className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700"
+							>
+								View car rental
+							</Link>
+						</div>
+					</div>
+				</div>
 			) : (
 				<div>
 					<div className="container mx-auto mt-10">
 						<div className="flex shadow-md my-10">
+							<TitleBarPage title="Car favorite cart" />
+							<ToastContainer className="toastify text-xs" />
 							<div className="w-full bg-white px-10 py-10">
 								<div className="flex justify-between border-b pb-8">
-									<h1 className="font-semibold text-2xl">Favorite card</h1>
+									<h1 className="font-semibold text-2xl">Car favorite cart</h1>
 									<h2 className="font-semibold text-2xl">3 Items</h2>
 								</div>
 								<div className="flex mt-10 mb-5">
@@ -65,57 +82,16 @@ const FavoriteCart = () => {
 										Total
 									</h3>
 								</div>
-								<div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
-									<div className="flex w-2/5">
-										<div className="w-20">
-											<img
-												className="h-24"
-												src="https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z"
-												alt=""
-											/>
-										</div>
-										<div className="flex flex-col justify-between ml-4 flex-grow">
-											<span className="font-bold text-sm">Iphone 6S</span>
-											<span className="text-red-500 text-xs">Apple</span>
-											<p className="font-semibold hover:text-red-500 text-gray-500 text-xs">
-												Remove
-											</p>
-										</div>
-									</div>
-									<div className="flex justify-center w-1/5">
-										<svg
-											className="fill-current text-gray-600 w-3"
-											viewBox="0 0 448 512"
-										>
-											<path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-										</svg>
 
-										<input
-											className="mx-2 border text-center w-8"
-											type="text"
-											placeholder="1"
+								{favoriteCartItems &&
+									favoriteCartItems.map((item) => (
+										<FavoriteItemCard
+											item={item}
+											removeCarsCart={removeCarsCart}
+											increaseQuantity={increaseQuantity}
+											decreaseQuantity={decreaseQuantity}
 										/>
-
-										<svg
-											className="fill-current text-gray-600 w-3"
-											viewBox="0 0 448 512"
-										>
-											<path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-										</svg>
-									</div>
-									<span className="text-center w-1/5 font-semibold text-sm">
-										$400.00
-									</span>
-									<span className="text-center w-1/5 font-semibold text-sm">
-										$400.00
-									</span>
-									<span className="text-center w-1/5 font-semibold text-sm">
-										$400.00
-									</span>
-									<span className="text-center w-1/5 font-semibold text-sm">
-										$400.00
-									</span>
-								</div>
+									))}
 								<div className="flex font-semibold text-xl mt-10 justify-center">
 									<p>
 										Total: 123 <PriceCheckIcon />
--- client/src/components/FavoriteCart/FavouriteItemCard/FavoriteItemCard.js
+++ client/src/components/FavoriteCart/FavouriteItemCard/FavoriteItemCard.js
@@ -3,21 +3,93 @@ import moment from 'moment';
 import { Link } from 'react-router-dom';
 import './FavoriteItemCard.css';
 
-export const FavoriteItemCard = ({ item, removeCarsCart }) => {
+export const FavoriteItemCard = ({
+	item,
+	removeCarsCart,
+	increaseQuantity,
+	decreaseQuantity,
+}) => {
 	function addDays(theDate, days) {
 		return new Date(theDate.getTime() + days * 24 * 60 * 60 * 1000);
 	}
-	let newDate = addDays(new Date(item.startDay), 2);
+
+	let newEndDate = addDays(new Date(item.startDay), item.quantity);
+
+	let a = item.rentPerDay;
+	console.log(a);
 	return (
-		<div>
-			<div className="CartItemCard">
-				<h4>Product: {item.name}</h4>
-				<h4>startDay: {item.startDay}</h4>
-				<h4>startDay: {moment(newDate).format('LLL')}</h4>
-				<h4>Available: {item.available}</h4>
-				<h4>{`Price: ₹${item.rentPerDay}`}</h4>
-				<button onClick={() => removeCarsCart(item.car)}>Remove</button>
+		// <div>
+		// 	<div className="CartItemCard">
+		// 		<h4>Product: {item.name}</h4>
+		// 		<h4>startDay: {item.startDay}</h4>
+		// 		<h4>startDay: {moment(newDate).format('LLL')}</h4>
+		// 		<h4>Available: {item.available}</h4>
+		// 		<h4>{`Price: ₹${item.rentPerDay}`}</h4>
+		// 		<button onClick={() => removeCarsCart(item.car)}>Remove</button>
+		// 	</div>
+		// </div>
+		<div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+			<div className="flex w-2/5">
+				<div className="w-20">
+					<img
+						className="h-24"
+						src="https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z"
+						alt=""
+					/>
+				</div>
+				<div className="flex flex-col justify-between ml-4 flex-grow">
+					<span className="font-bold text-sm">{item.name}</span>
+					<span className="text-red-500 text-xs">
+						{item.seatsCategory} seat category
+					</span>
+					<p
+						className="font-semibold hover:text-red-500 text-gray-500 text-xs"
+						onClick={() => removeCarsCart(item.car)}
+					>
+						Remove
+					</p>
+				</div>
 			</div>
+			<div className="flex justify-center w-1/5">
+				<button
+					className="font-semibold hover:text-red-500 text-gray-500 text-xs"
+					onClick={() => decreaseQuantity(item.car, item.quantity)}
+				>
+					-
+				</button>
+
+				<input
+					className="mx-2 border text-center w-8"
+					type="number"
+					value={item.quantity}
+					readOnly
+				/>
+				<button
+					className="font-semibold hover:text-red-500 text-gray-500 text-xs"
+					onClick={() =>
+						increaseQuantity(item.car, item.quantity, item.available)
+					}
+				>
+					+
+				</button>
+			</div>
+			<span className="text-center w-1/5 font-semibold text-sm">
+				$ {item.rentPerDay}
+			</span>
+			<span className="text-center w-1/5 font-semibold text-sm">
+				{item.startDay}
+			</span>
+			<span className="text-center w-1/5 font-semibold text-sm">
+				{moment(newEndDate).format('LLL')}
+			</span>
+
+			{/* <span className="text-center w-1/5 font-semibold text-sm">{`$${item.rentPerDay.reduce(
+				(all, item) => all + item.rentPerDay * item.quantity,
+				0
+			)}`}</span> */}
+			<span className="text-center w-1/5 font-semibold text-sm">
+				$ {item.rentPerDay * item.quantity}
+			</span>
 		</div>
 	);
 };
